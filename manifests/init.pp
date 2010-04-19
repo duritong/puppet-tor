f56a786e8ff345a2f1679339b2a2c51294975582
Silvio Rhatto <rhatto@riseup.net>
2010-04-18 22:26:05 -0300

    Managing /etc/polipo folder

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,6 +13,13 @@ class tor {
     require => Package["polipo"],
   }
 
+  file { "/etc/polipo":
+    ensure => directory,
+    owner  => root,
+    group  => root,
+    mode   => 0755,
+  }
+
   file { "/etc/polipo/config":
     ensure  => present,
     owner   => root,
@@ -20,5 +27,6 @@ class tor {
     mode    => 0644,
     source  => "puppet://$server/modules/tor/polipo.conf",
     notify  => Service["polipo"],
+    require => File["/etc/polipo"],
   }
 }
