3fb09572cbbef20a72feb3e3a50711ba7eba1cea
mh <mh@immerda.ch>
2013-01-02 13:41:56 +0100

    split out defines into their own files

---
+++ manifests/daemon/map_address.pp
@@ -0,0 +1,16 @@
+# map address definition
+define tor::daemon::map_address(
+  $address    = '',
+  $newaddress = '') {
+
+  concat::fragment { "08.map_address.${name}":
+    ensure  => $ensure,
+    content => template('tor/torrc.map_address.erb'),
+    owner   => 'debian-tor',
+    group   => 'debian-tor',
+    mode    => '0644',
+    order   => 08,
+    target  => $tor::daemon::config_file,
+  }
+}
+
