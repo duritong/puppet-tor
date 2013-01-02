e5a89865b70d694ebf978ed9743476b511974d92
mh <mh@immerda.ch>
2013-01-02 13:59:50 +0100

    use the deploy mechanism of the munin module

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -1,30 +1,14 @@
 # munin plugins for puppet
 class tor::munin {
-
-  file {
-    '/usr/local/share/munin-plugins/tor_connections':
-      source => 'puppet:///modules/tor/munin/tor_connections',
-      mode   => '0755',
-      owner  => root,
-      group  => root;
-
-    '/usr/local/share/munin-plugins/tor_routers':
-      source => 'puppet:///modules/tor/munin/tor_routers',
-      mode   => '0755',
-      owner  => root,
-      group  => root;
-
-    '/usr/local/share/munin-plugins/tor_traffic':
-      source => 'puppet:///modules/tor/munin/tor_traffic',
-      mode   => '0755',
-      owner  => root,
-      group  => root;
+  Munin::Plugin::Deploy {
+    config  => "user debian-tor\n env.cookiefile /var/run/tor/control.authcookie"
   }
-
-  munin::plugin {
-    [ 'tor_connections', 'tor_routers', 'tor_traffic' ]:
-      ensure         => present,
-      config         => "user debian-tor\n env.cookiefile /var/run/tor/control.authcookie",
-      script_path_in => '/usr/local/share/munin-plugins';
+  munin::plugin::deploy {
+    'tor_connections':
+      source => 'tor/munin/tor_connections';
+    'tor_routers':
+      source => 'tor/munin/tor_routers';
+    'tor_traffic':
+      source => 'tor/munin/tor_traffic';
   }
 }
