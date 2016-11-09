6deb959721ba5e90d876ff34343c0926730faf7b
mh <mh@immerda.ch>
2016-11-09 23:05:37 +0100

    set it to a port allowed by munin

--- manifests/munin.pp
+++ manifests/munin.pp
@@ -2,18 +2,18 @@
 class tor::munin {
   tor::daemon::control{
     'control_port_for_munin':
-      port                  => 19051,
+      port                  => 9001,
       cookie_authentication => 1,
       cookie_auth_file      => '/var/run/tor/control.authcookie',
   }
 
   include ::tor::daemon::params
   Munin::Plugin::Deploy {
-    config  => "user ${tor::daemon::params::user}\n env.cookiefile /var/run/tor/control.authcookie\n env.port 19051"
+    config => "user ${tor::daemon::params::user}\n env.cookiefile /var/run/tor/control.authcookie\n env.port 9001"
   }
   munin::plugin::deploy {
     'tor_openfds':
-      config  => 'user root',
+      config => 'user root',
       source => 'tor/munin/tor_openfds';
     'tor_routers':
       source => 'tor/munin/tor_routers';
