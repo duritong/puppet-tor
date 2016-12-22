054c5311fbe235033e325bf68637615d581355aa
guido <guido@dis.tur.bio>
2016-12-21 21:16:12 -0300

    support for single_hop onion services added

--- manifests/daemon/hidden_service.pp
+++ manifests/daemon/hidden_service.pp
@@ -1,15 +1,13 @@
 # hidden services definition
 define tor::daemon::hidden_service(
-  $ports    = [],
-  $data_dir = $tor::daemon::data_dir,
-  $ensure   = present ) {
+  $ports      = [],
+  $single_hop = false,
+  $data_dir   = $tor::daemon::data_dir,
+  $ensure     = present ) {
 
   concat::fragment { "05.hidden_service.${name}":
     ensure  => $ensure,
     content => template('tor/torrc.hidden_service.erb'),
-    owner   => 'debian-tor',
-    group   => 'debian-tor',
-    mode    => '0644',
     order   => 05,
     target  => $tor::daemon::config_file,
   }
