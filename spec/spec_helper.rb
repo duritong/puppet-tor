10767a0016b0f46aceab9ac97738390035a112dc
Louis-Philippe Véronneau <pollito@riseup.net>
2017-10-11 15:52:10 -0400

    copy CI infra from the shared apt module as-is

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+# https://puppetlabs.com/blog/testing-modules-in-the-puppet-forge
+require 'puppetlabs_spec_helper/module_spec_helper'
+
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+
+RSpec.configure do |c|
+
+  c.manifest_dir = File.join(fixture_path, 'manifests')
+  c.module_path  = File.join(fixture_path, 'modules')
+
+  c.color = true
+end
