ff149a1c48e988f2c8ad8e69b582b0c6c0895401
mh <mh@immerda.ch>
2018-02-23 23:30:01 +0100

    keep the syntax of config snippes consistent

--- manifests/daemon/transport_plugin.pp
+++ manifests/daemon/transport_plugin.pp
@@ -1,13 +1,16 @@
 # transport plugin
 define tor::daemon::transport_plugin(
+  $ensure                     = 'present',
   $servertransport_plugin     = '',
   $servertransport_listenaddr = '',
   $servertransport_options    = '',
-  $ext_port                   = '' ) {
-
-  concat::fragment { '11.transport_plugin':
-    content => template('tor/torrc.transport_plugin.erb'),
-    order   => 11,
-    target  => $tor::daemon::config_file,
+  $ext_port                   = '',
+) {
+  if $ensure == 'present' {
+    concat::fragment { '11.transport_plugin':
+      content => template('tor/torrc.transport_plugin.erb'),
+      order   => 11,
+      target  => $tor::daemon::config_file,
+    }
   }
 }
