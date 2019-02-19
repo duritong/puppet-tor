092883c3b6081158d180774e41b31cea729f034b
Louis-Philippe Véronneau <pollo@debian.org>
2019-02-19 14:00:53 -0500

    epp for socks

--- manifests/daemon/socks.pp
+++ manifests/daemon/socks.pp
@@ -1,12 +1,15 @@
 # socks definition
 define tor::daemon::socks(
-  $ensure   = 'present',
-  $port     = 0,
-  $policies = [],
+  Enum['present', 'absent'] $ensure = 'present',
+  Stdlib::Port $port  = 0,
+  Array[String] $policies,
 ) {
   if $ensure == 'present' {
     concat::fragment { '02.socks':
-      content => template('tor/torrc/02_socks.erb'),
+      content => epp('tor/torrc/02_socks.epp', {
+        'port'     => $tor::daemon::socks::port,
+        'policies' => $tor::daemon::socks::policies,
+      }),
       order   => '02',
       target  => $tor::daemon::config_file,
     }
