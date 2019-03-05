d1018b766e98a557706ea3165d7cf65ce3b10e29
Louis-Philippe Véronneau <pollo@debian.org>
2019-03-05 17:07:52 -0500

    Don't force the installation of tor-geoipdb for Debian. The tor package recommends it anyway, and if people explicitely disabled recommends, we d'ont want to install it

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,12 +3,6 @@ class tor::install {
   package { 'tor':
     ensure => $tor::version,
 
-    if $facts['osfamily'] == 'Debian' {
-      'tor-geoipdb':
-        ensure => $tor::version,
-        before => Service['tor'],
-    }
-
     if $tor::arm {
       'tor-arm':
         ensure => $tor::version,
