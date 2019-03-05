28cd9e88164e89e2ec4dd96f973ce31fae5927d7
Louis-Philippe Véronneau <pollo@debian.org>
2019-03-05 15:43:46 -0500

    epp for exit_policies

--- manifests/daemon/exit_policy.pp
+++ manifests/daemon/exit_policy.pp
@@ -1,13 +1,18 @@
 # exit policies
 define tor::daemon::exit_policy(
-  $ensure         = 'present',
-  $accept         = [],
-  $reject         = [],
-  $reject_private = 1,
+  Enum['present', 'absent'] $ensure = 'present',
+  Optional[Array[String]] $accept   = undef,
+  Optional[Array[String]] $reject   = undef,
+  Boolean $reject_private           = true,
 ) {
   if $ensure == 'present' {
     concat::fragment { "07.exit_policy.${name}":
-      content => template('tor/torrc/07_exit_policy.erb'),
+      content => epp('tor/torrc/07_exit_policy.epp', {
+        'name'           => $name,
+        'accept'         => $tor::daemon::exit_policy::accept,
+        'reject'         => $tor::daemon::exit_policy::reject,
+        'reject_private' => $tor::daemon::exit_policy::reject_private,
+      }),
       order   => '07',
       target  => $tor::daemon::config_file,
     }
