325b6161ee1ce4c74ccd9fb0f858298fa1336527
Louis-Philippe Véronneau <pollo@debian.org>
2019-03-05 16:07:17 -0500

    epp for transparent

--- manifests/daemon/transparent.pp
+++ manifests/daemon/transparent.pp
@@ -1,14 +1,15 @@
 # Transparent proxy definition
 define tor::daemon::transparent(
-  $ensure           = 'present',
-  $port             = 0) {
-
+  Enum['present', 'absent'] $ensure = 'present',
+  Variant[0, Stdlib::Port] $port    = 0,
+){
   if $ensure == 'present' {
     concat::fragment { "10.transparent.${name}":
-      content => template('tor/torrc/10_transparent.erb'),
+      content => epp('tor/torrc/10_transparent.epp', {
+        'port' => $tor::daemon::transparent::port,
+      }),
       order   => '10',
       target  => $tor::daemon::config_file,
     }
   }
 }
-
