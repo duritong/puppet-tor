df3365d343d9ed84c895482dde33aa88a88e144f
Louis-Philippe Véronneau <pollo@debian.org>
2019-03-05 15:57:51 -0500

    epp for map_address

--- manifests/daemon/map_address.pp
+++ manifests/daemon/map_address.pp
@@ -1,12 +1,16 @@
 # map address definition
 define tor::daemon::map_address(
-  Enum['present', 'absent'] $ensure = 'present',
-  Stdlib::Host $address             = '',
-  Stdlib::Host $newaddress          = '',
+  Enum['present', 'absent'] $ensure  = 'present',
+  Optional[Stdlib::Host $address]    = undef,
+  Optional[Stdlib::Host $newaddress] = undef,
 ) {
   if $ensure == 'present' {
     concat::fragment { "09.map_address.${name}":
-      content => template('tor/torrc/09_map_address.erb'),
+      content => epp('tor/torrc/09_map_address.epp' {
+        'name'       => $name,
+        'address'    => $tor::daemon::map_address::address,
+        'newaddress' => $tor::daemon::map_address::newaddress,
+      }),
       order   => '09',
       target  => $tor::daemon::config_file,
     }
