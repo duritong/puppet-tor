6c1f44ec196fb747ea1c8e5f04f8d71e72509628
Louis-Philippe Véronneau <pollo@debian.org>
2019-03-05 15:52:23 -0500

    epp for dns

--- manifests/daemon/dns.pp
+++ manifests/daemon/dns.pp
@@ -1,14 +1,15 @@
 # DNS definition
 define tor::daemon::dns(
   Enum['present', 'absent'] $ensure = 'present',
-  Stdlib::Port $port  = 0,
+  Variant[0, Stdlib::Port] $port    = 0,
 ){
   if $ensure == 'present' {
     concat::fragment { "08.dns.${name}":
-      content => template('tor/torrc/08_dns.erb'),
+      content => epp('tor/torrc/08_dns.epp' {
+        'port' => $tor::daemon::dns::port,
+      }),
       order   => '08',
       target  => $tor::daemon::config_file,
     }
   }
 }
-
