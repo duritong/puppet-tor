89ebceb540caa8caef60fb531f085aacfb09256e
Louis-Philippe Véronneau <pollo@debian.org>
2019-03-26 15:53:20 -0400

    test transport_plugin

--- manifests/daemon/transport_plugin.pp
+++ manifests/daemon/transport_plugin.pp
@@ -4,15 +4,15 @@ define tor::daemon::transport_plugin(
   Optional[String] $servertransport_plugin     = undef,
   Optional[String] $servertransport_listenaddr = undef,
   Optional[String] $servertransport_options    = undef,
-  Optional[Tor::Port] $ext_port                = undef,
+  Optional[Enum['auto', Tor::Port]] $ext_port  = undef,
 ) {
   if $ensure == 'present' {
     concat::fragment { '12.transport_plugin':
       content => epp('tor/torrc/12_transport_plugin.epp', {
-        'servertransport_plugin'     => $tor::daemon::transport_plugin::servertransport_plugin,
-        'servertransport_listenaddr' => $tor::daemon::transport_plugin::servertransport_listenaddr,
-        'servertransport_options'    => $tor::daemon::transport_plugin::servertransport_options,
-        'ext_port'                   => $tor::daemon::transport_plugin::ext_port,
+        'servertransport_plugin'     => $servertransport_plugin,
+        'servertransport_listenaddr' => $servertransport_listenaddr,
+        'servertransport_options'    => $servertransport_options,
+        'ext_port'                   => $ext_port,
       }),
       order   => 12,
       target  => $tor::config_file,
