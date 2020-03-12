d16126484962ae0bf27503a623c17691515f5658
tr <tr@netlab.li>
2020-03-12 21:15:26 +0100

    Add the AutomapHostsOnResolve defintion.

---
+++ manifests/daemon/automaphostsonresolve.pp
@@ -0,0 +1,15 @@
+# AutomapHostsOnResolve definition
+define tor::daemon::automaphostsonresolve(
+  Enum['present','absent'] $ensure  = 'present',
+  Boolean $automap_hosts_on_resolve = false,
+){
+  if $ensure == 'present' {
+    concat::fragment { "13.automaphostsonresolve.${name}":
+      content => epp('tor/torrc/13_automaphostsonresolve.epp', {
+        'automap_hosts_on_resolve' => $automap_hosts_on_resolve,
+      }),
+      order   => '13',
+      target  => $tor::config_file,
+    }
+  }
+}
