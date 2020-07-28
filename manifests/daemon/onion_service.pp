62f86585da8ae0ea517517de4b11ed3489f88e86
mh <mh@immerda.ch>
2020-07-28 20:55:12 +0200

    don't deploy key if we want absent

--- manifests/daemon/onion_service.pp
+++ manifests/daemon/onion_service.pp
@@ -66,37 +66,44 @@ define tor::daemon::onion_service(
     if $private_key and ($private_key_name and $private_key_store_path) {
       fail('Either private_key OR (private_key_name AND private_key_store_path) must be set, but not all three of them')
     }
-    if $private_key_store_path and $private_key_name {
-      $tmp = tor::generate_onion_key($private_key_store_path,$private_key_name)
-      $os_hostname = $tmp[0]
-      $real_private_key = $tmp[1]
-    } else {
-      $os_hostname = tor::onion_address($private_key)
-      $real_private_key = $private_key
-    }
-    include ::tor::daemon::params
     file{
       $data_dir_path:
-        ensure  => directory,
         purge   => true,
         force   => true,
-        recurse => true,
+        recurse => true;
+    }
+    if $ensure == 'present' {
+      include ::tor::daemon::params
+      File[$data_dir_path]{
+        ensure  => directory,
         owner   => $tor::daemon::params::user,
         group   => $tor::daemon::params::group,
         mode    => '0600',
-        require => Package['tor'];
-      "${data_dir_path}/private_key":
-        content => $real_private_key,
-        owner   => $tor::daemon::params::user,
-        group   => $tor::daemon::params::group,
-        mode    => '0600',
-        notify  => Service['tor'];
-      "${data_dir_path}/hostname":
-        content => "${os_hostname}.onion\n",
-        owner   => $tor::daemon::params::user,
-        group   => $tor::daemon::params::group,
-        mode    => '0600',
-        notify  => Service['tor'];
+        require => Package['tor'],
+      }
+      if $private_key_store_path and $private_key_name {
+        $tmp = tor::generate_onion_key($private_key_store_path,$private_key_name)
+        $os_hostname = $tmp[0]
+        $real_private_key = $tmp[1]
+      } else {
+        $os_hostname = tor::onion_address($private_key)
+        $real_private_key = $private_key
+      }
+      file{
+        default:
+          owner   => $tor::daemon::params::user,
+          group   => $tor::daemon::params::group,
+          mode    => '0600',
+          notify  => Service['tor'];
+        "${data_dir_path}/private_key":
+          content => $real_private_key;
+        "${data_dir_path}/hostname":
+          content => "${os_hostname}.onion\n";
+      }
+    } else {
+      File[$data_dir_path]{
+        ensure => absent
+      }
     }
   }
 }
