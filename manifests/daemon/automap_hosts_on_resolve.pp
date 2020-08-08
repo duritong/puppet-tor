2a180c2c4e8e0db1e56216c822d4604a045786ae
Louis-Philippe Véronneau <pollo@debian.org>
2020-08-08 17:46:46 -0400

    Document the new automap_hosts_on_resolve define

--- manifests/daemon/automap_hosts_on_resolve.pp
+++ manifests/daemon/automap_hosts_on_resolve.pp
@@ -1,4 +1,12 @@
-# AutomapHostsOnResolve definition
+# @summary Extend basic Tor configuration with a snippet based configuration.
+#          Automap Hosts on Resolve module.
+#
+# @param ensure
+#   Whether this module should be used or not.
+#
+# @param automap_hosts_on_resolve
+#   Whether AutomapHostsOnResolve should be enabled or not.
+#
 define tor::daemon::automap_hosts_on_resolve(
   Enum['present','absent'] $ensure  = 'present',
   Boolean $automap_hosts_on_resolve = false,
